5c20619dc635a34eb7cbf004aa66449bc0dd46a1
jnorthr <james.b.northrop@googlemail.com>
2017-10-22 15:49:51 +0200

    fix Scala Stack[] test module

--- templates/test/NailTest.scala
+++ templates/test/NailTest.scala
@@ -22,7 +22,7 @@ import org.scalatest._
 class ${classname}Spec extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
+    val stack = new Stack[Int]()
     stack.push(1)
     stack.push(2)
     stack.pop() should be (2)
@@ -30,7 +30,7 @@ class ${classname}Spec extends FlatSpec with Matchers {
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
+    val emptyStack = new Stack[Int]()
     a [NoSuchElementException] should be thrownBy {
       emptyStack.pop()
     } 
