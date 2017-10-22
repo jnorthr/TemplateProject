2a07a60c2bb69602d76151309302256a75184969
jnorthr <james.b.northrop@googlemail.com>
2017-10-22 16:03:28 +0200

    fix Stack syntax" ;

--- templates/test/NailTest.scala
+++ templates/test/NailTest.scala
@@ -22,15 +22,15 @@ import org.scalatest._
 class ${classname}Spec extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]()
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    val stack = new Stack[String]()
+    stack.push("abc")
+    stack.push("xyz")
+    stack.pop() should be ("xyz")
+    stack.pop() should be ("abc")
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]()
+    val emptyStack = new Stack[String]()
     a [NoSuchElementException] should be thrownBy {
       emptyStack.pop()
     } 
