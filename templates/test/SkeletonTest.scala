38883a0016206f4aef606cf4a75dee2076206b2c
jnorthr <james.b.northrop@googlemail.com>
2017-10-23 15:31:59 +0200

    fix Scala Stack example

--- templates/test/SkeletonTest.scala
+++ templates/test/SkeletonTest.scala
@@ -23,10 +23,10 @@ class ${classname}Spec extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = Stack[String]()
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    stack.push("1")
+    stack.push("2")
+    stack.pop() should be ("2")
+    stack.pop() should be ("1")
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
