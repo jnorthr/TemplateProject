bb43f08cf2bb29a35776f89dcdbfaef43ae67520
jnorthr <james.b.northrop@googlemail.com>
2017-10-23 15:22:42 +0200

    fix scala syntax

--- templates/test/SkeletonTest.scala
+++ templates/test/SkeletonTest.scala
@@ -22,7 +22,7 @@ import org.scalatest._
 class ${classname}Spec extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
+    val stack = Stack[String]()
     stack.push(1)
     stack.push(2)
     stack.pop() should be (2)
@@ -30,7 +30,7 @@ class ${classname}Spec extends FlatSpec with Matchers {
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
+    val emptyStack = Stack[String]()
     a [NoSuchElementException] should be thrownBy {
       emptyStack.pop()
     } 
